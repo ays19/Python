146e583b562088748548e5277d206d614772d4bf
ays19 <sahsanyasir@gmail.com>
2024-12-18 08:17:11 +0600

    iteration

---
+++ 2.Iteration.py
@@ -0,0 +1,20 @@
+
+# for sha in range(1,20,2):  #(start,end,step)
+#  print(sha)
+
+# s = "Red yellow blue Red yellow blue Red yellow blue"
+# list1 = s.split(" ")
+# count = 0
+# for i in list1:
+#  if i == "Red":
+#   count+=1
+# print(count)
+
+
+sum = 0
+for ays in range(5):
+ num = int(input())
+ sum = sum + num
+
+avg = sum/5
+print(avg)
