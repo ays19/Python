58f7e4a68e74d6891810a5e789ca7bc2505787e8
ays19 <sahsanyasir@gmail.com>
2024-12-19 19:17:08 +0600

    slicing

---
+++ 3.String.py
@@ -0,0 +1,14 @@
+name ="Sharar"
+name1 = "123"
+name2 = "@#$"
+
+print(name, name1, name2)
+print(name+name1+name2)
+
+print(name[0])
+print(name[len(name)-1])
+print(name[-1])
+print(name[-len(name)])
+print(name[0]+name[1]+name[2])
+print(name[1:4]) #Slicing
+print(name[1:4]+name[-6:]+name1[:2])
