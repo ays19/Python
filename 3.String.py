cdbb61bb42fd943de6b0f3fea8225e564f01abae
ays19 <sahsanyasir@gmail.com>
2024-12-20 07:52:34 +0600

    list

--- 3.String.py
+++ 3.String.py
@@ -2,22 +2,32 @@ name ="Sharar"
 name1 = "123"
 name2 = "@#$"
 
-print(name, name1, name2)
-print(name+name1+name2)
+# print(name, name1, name2)
+# print(name+name1+name2)
 
-print(name[0])
-print(name[len(name)-1])
-print(name[-1])
-print(name[-len(name)])
-print(name[0]+name[1]+name[2])
-print(name[1:4]) #Slicing
-print(name[1:4]+name[-6:]+name1[:2])
+# print(name[0])
+# print(name[len(name)-1])
+# print(name[-1])
+# print(name[-len(name)])
+# print(name[0]+name[1]+name[2])
+# print(name[1:4]) #Slicing
+# print(name[1:4]+name[-6:]+name1[:2])
 
-s= "Hello"
-for i in s:
- if(i=="l"):
-  print("Yes")
-  break
+# s= "Hello"
+# for i in s:
+#  if(i=="l"):
+#   print("Yes")
+#   break
 
-print("l" in s)
+# print("l" in s)
+
+X= "ahsan Yasir Sharar"
+idx = 0
+for i in range(0, len(X)):
+ if X[i]== "Y":
+   idx= i
+   break
+print(i)
+print(X[0:2]+X[idx:idx+2])
+
 
---
+++ 3.list_3.py
@@ -0,0 +1,4 @@
+L1= [5,6,7, True, ["HI", 5]]
+print(L1)
+L1.append(10)
+print(L1)
