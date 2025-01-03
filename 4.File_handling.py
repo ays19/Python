59d6f3c6e185f5c4f198d27cfeba6dc3779dae1b
ays19 <sahsanyasir@gmail.com>
2025-01-03 20:09:08 +0600

    file

--- 4.File_handling.py
+++ 4.File_handling.py
@@ -3,5 +3,14 @@
 # f.close()
 
 with open("test.txt" , "r") as f:
-    print(f.read())
-    (f.read())
+    #print(f.read())
+    #print(f.read(50))
+    #print("yyy\n")
+    #print(f.read(20))
+    #print(f.readline())
+    count=1
+    for i in f:    
+      if count>4:
+        break
+      print(i)
+      count+=1
