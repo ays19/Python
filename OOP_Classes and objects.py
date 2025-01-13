255a34c09663335e83ac8bd242c0203be487ec02
ays19 <sahsanyasir@gmail.com>
2025-01-13 23:12:02 +0600

    method

--- OOP_Classes and objects.py
+++ OOP_Classes and objects.py
@@ -1,11 +1,16 @@
 class patient:
-    def __init__(self, name, age, bg): # Constructor
-        self.name = name
+    def __init__(self, n, age, bg): # Constructor
+        self.name = n  # Instance variable
         self.age = age  # Instance variable
         self.bg = bg
+
+    def printInfo(self):
+        print(f"Name: {self.name}\nAge: {self.age}\nBlood Group: {self.bg}")
         
 p1 = patient("John", 25, "O+")
 p2 = patient("Amin", 21, "AB-")
 
 print(p1.name, p1.age, p1.bg)
 print(p2.name)
+print("000000")
+p1.printInfo()
