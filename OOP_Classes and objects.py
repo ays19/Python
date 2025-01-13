f0bc39fc86a39602147769656e9ed06de9edb6b2
ays19 <sahsanyasir@gmail.com>
2025-01-13 23:01:28 +0600

    object constructor

--- OOP_Classes and objects.py
+++ OOP_Classes and objects.py
@@ -1,3 +1,11 @@
 class patient:
-    def __init__(self):
-        pass
+    def __init__(self, name, age, bg): # Constructor
+        self.name = name
+        self.age = age  # Instance variable
+        self.bg = bg
+        
+p1 = patient("John", 25, "O+")
+p2 = patient("Amin", 21, "AB-")
+
+print(p1.name, p1.age, p1.bg)
+print(p2.name)
