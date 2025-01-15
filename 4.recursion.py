dd0f54d4e2dac765ccb4e8c9f5d5e61a86d5bdf8
ays19 <sahsanyasir@gmail.com>
2025-01-15 23:04:11 +0600

    recursion

--- 4.recursion.py
+++ 4.recursion.py
@@ -1,6 +1,25 @@
 def mA(x):
     print(x)
-    if x<=20:
-        mA(x+1)
+    if x <= 20:  # condition to stop the recursion
+        mA(x + 1)  # Recursive call
 
-mA(10)        
+mA(10)     
+print("000000")
+def mA(x):
+    
+    if x <= 20:  # condition to stop the recursion
+        print(x)
+        mA(x + 1)  # Recursive call
+
+mA(10) 
+
+print("000000")
+
+def mA(x):
+    
+    if x <= 20:  # condition to stop the recursion
+        
+        mA(x + 1)  # Recursive call
+        print(x)
+
+mA(10)  
