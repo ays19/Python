c984ed231ad2a78a2891abf3a317ba60a888b2b6
ays19 <sahsanyasir@gmail.com>
2025-01-16 13:01:47 +0600

    recursion

--- 4.recursion_pblm_solve.py
+++ 4.recursion_pblm_solve.py
@@ -12,3 +12,19 @@ print("000000")
 
 for i in s[::-1]: #reverse order another way
     print(i)
+
+print("000000") 
+
+def rcs(s):  #recursive function
+    if len(s)>0:
+        print(s[0])
+        rcs(s[1:])
+
+rcs("Assalamualikumyou") #recursive call
+
+def rcs(s):  #recursive function
+    if len(s)>0:
+        rcs(s[1:])
+        print(s[0])
+
+rcs("Assalamualikumyou") #recursive call
